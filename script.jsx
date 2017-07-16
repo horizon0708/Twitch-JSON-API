b0db9956f840ff231b8795e6fb72f99d9f2733b8
James Kim <james.kim.nz92@gmail.com>
2017-07-17 11:21:34 +1200

    working on async map

--- script.jsx
+++ script.jsx
@@ -5,7 +5,8 @@
 // User Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
 
 async function getJSON(categ, name) {
-    var data = $.getJSON('https://wind-bow.gomix.me/twitch-api/' + categ + '/' + name + '?callback=?', function (data) {;
+    var data = $.getJSON('https://wind-bow.gomix.me/twitch-api/' + categ + '/' + name + '?callback=?', function (data) {
+        ;
     });
     return data;
 }
@@ -55,86 +56,122 @@ class Main extends React.Component {
     }
 
     componentWillMount() {
-        this.setState({ showing: "ALL"});
+        this.setState({ showing: "ALL" });
         this.getStreamStatus();
     }
 
-    getStreamStatus(){
+    getStreamStatus() {
         async.filter(this.state.all, isOnline,
             (err, result) => {
-                this.setState({ online: result });
+                this.setState({online: result});
+                //console.log(result);
+                //this.loadStreamData(result);
             });
         async.filter(this.state.all, isOffline,
             (err, result) => {
                 this.setState({ offline: result });
             });
     }
-
-    changeMode(e, props){
-        this.setState({showing: props});
+    loadStreamData(r){
+        // make an array of strings into objects
+        async.map(r, function (item, callback) {
+            return item += "wow";
+        }, (err, result) => {
+            this.setState({online: result});
+            console.log(result);
+        })
     }
 
-    addStreamer(){
-        if (this.state.validationIsLoading === false && this.state.validation === "valid"){
+    changeMode(e, props) {
+        this.setState({ showing: props });
+    }
+
+    addStreamer() {
+        if (this.state.validationIsLoading === false && this.state.validation === "valid") {
             let newArr = this.state.all.concat(this.state.query);
-            this.setState({all: newArr});
+            this.setState({ all: newArr });
             this.getStreamStatus();
         }
     }
 
-    handleInput(e){
+    handleInput(e) {
         var inputVal = e.target.value;
-        this.setState({validationIsLoading: true});
-        this.setState({query: e.target.value}, () => {
-            getJSON("users", this.state.query).then((d)=> {
-                if(this.state.query !== inputVal) { return; }
-                if (d.message){
-                    this.setState({validationMsg: d.message,
-                                   validation: "invalid",
-                                    userinfo: ""});
+        this.setState({ validationIsLoading: true });
+        this.setState({ query: e.target.value }, () => {
+            getJSON("users", this.state.query).then((d) => {
+                if (this.state.query === "") { this.setState({ userinfo: "" }) }
+                if (this.state.query !== inputVal) { return; }
+                if (d.message) {
+                    this.setState({
+                        validationMsg: d.message,
+                        validation: "invalid",
+                        userinfo: "",
+                        validationIsLoading: false
+                    });
                 }
-                if (d.name){
-                    this.setState({validation: "valid",
-                                    userinfo: d});
+                if (d.name) {
+                    this.setState({
+                        validation: "valid",
+                        userinfo: d,
+                        validationIsLoading: false
+                    });
                 }
-                this.setState({validationIsLoading: false});
-                
             });
         })
     }
 
-    mapStreamerList(){
-        if (this.state.showing === "ALL"){
-            return this.state.all.map(x => <li>{x}</li>)
-        }
-        if (this.state.showing === "ONLINE"){
+    mapStreamerList() {
+        if (this.state.showing === "ALL") {
+            return this.state.all.map(x => {
+                return <li>{x}</li>;
+        })}
+        if (this.state.showing === "ONLINE") {
             return this.state.online.map(x => <li>{x}</li>)
         }
         return this.state.offline.map(x => <li>{x}</li>)
     }
 
-    render() {
-        return <div>
-            <div>
-                <input onChange={(e) => this.handleInput(e)} value={this.state.query} />
-                <i>{this.state.validation}</i> 
-                <i>{this.state.validationIsLoading}</i>
-                <button onClick={(e)=> this.addStreamer(e)}>add</button>
-                <div>
-                    <span>{this.state.userinfo.name}</span>
-                    <span>{this.state.userinfo.bio}</span>
+    userInfoCard() {
+        if (this.state.validation === "valid" && this.state.validationIsLoading === false) {
+            return <div className="card horizontal small">
+                <div className="card-image">
                     <img src={this.state.userinfo.logo} />
                 </div>
+                <div className="card-stacked">
+                    <div className="card-content">
+                        <h2>{this.state.userinfo.name}</h2>
+                        <p>{this.state.userinfo.bio}</p>
+                    </div>
+                    <div className="card-action">
+                        <a onClick={(e) => this.addStreamer(e)}>Add This Streamer to the List!</a>
+                    </div>
+                </div>
             </div>
-            <button onClick={(e) => this.changeMode(e, "ALL")}>All</button>
-            <button onClick={(e) => this.changeMode(e, "ONLINE")}>Online</button>
-            <button onClick={(e) => this.changeMode(e, "OFFLINE")}>Offline</button>
-            {this.mapStreamerList()}
+        }
+        return;
+    }
+
+    render() {
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col m6 s12 ">
+                    <input onChange={(e) => this.handleInput(e)} value={this.state.query} />
+                    <i>{this.state.validation}</i>
+                    <i>{this.state.validationIsLoading}</i>
+                    <button onClick={(e) => this.addStreamer(e)}>add</button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col m12 s12 results">
+                    {this.userInfoCard()}
+                    <button onClick={(e) => this.changeMode(e, "ALL")}>All</button>
+                    <button onClick={(e) => this.changeMode(e, "ONLINE")}>Online</button>
+                    <button onClick={(e) => this.changeMode(e, "OFFLINE")}>Offline</button>
+                    {this.mapStreamerList()}
+                </div>
+            </div>
         </div>
     }
 }
 
-
-
 ReactDOM.render(<Main />, document.getElementById('root'));
-      
